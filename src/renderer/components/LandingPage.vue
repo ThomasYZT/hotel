f8e0b3da638a194219e2abe21adfe71b95330af2
thomasYZT <624524240@qq.com>
2020-08-28 00:08:53 +0800

    first release

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          ThomasYZT
         </span>
         <system-information></system-information>
       </div>
